e4e18389074d0a78c946cd397324e09ee0b66a7f
Emanuele Sabetta <Emasoft@users.noreply.github.com>
2024-10-26 00:59:10 +0200

    delete script

--- clean-files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Search for email in all files
-find . -type f -not -path "./.git/*" -exec grep -l "713559+Emasoft@users.noreply.github.com" {} \;
-
-# Replace email in found files
-find . -type f -not -path "./.git/*" -exec sed -i 's/713559+Emasoft@users.noreply.github.com/713559+Emasoft@users.noreply.github.com/g' {} \;
-
-echo "Files cleaned of private email references"
